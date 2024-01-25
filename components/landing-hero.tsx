9901dbc0e6962805ace08ac999f1ef1e8d1aec2b
macuser <fz221@ic.ac.uk>
2024-01-25 10:40:07 +0000

    Changed 'Best' with 'Pioneering' in 'Best Speed-Learning Platform for' in the landing page.

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -10,7 +10,7 @@ export const LandingHero = () => {
   return (
     <div className="text-white fond-bold py-20 text-center space-y-8">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>Best Speed-Learning Platform for</h1>
+        <h1>Pioneering Speed-Learning Platform for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-green-200 to-green-600">
           <TypewriterComponent
             options={{
