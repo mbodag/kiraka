5d3f847b48b18c985274e6c6a6fe1d99098940b7
macuser <fz221@ic.ac.uk>
2024-01-25 16:41:27 +0000

    Added layer height=1.2 for 'Our Pricing Plans' to prevent cutoff of 'g'.

--- app/(landing)/(routes)/pricing/page.tsx
+++ app/(landing)/(routes)/pricing/page.tsx
@@ -5,7 +5,7 @@ const PricingPage = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="text-center" style={{ marginTop: '-450px' }}>
-        <h1 className="text-white text-7xl font-bold mb-8 typewriter" style={{ color: '#8FBC8F' }}>Our Pricing Plans</h1>
+        <h1 className="text-white text-7xl font-bold mb-8 typewriter" style={{ color: '#8FBC8F', lineHeight: '1.2'}}>Our Pricing Plans</h1>
         <p className="text-white text-xl max-w-2xl mx-auto animate-gradientFadeIn">
           Our platform is completely free! Our team wants you to get the most 
           out of your reading experience without breaking the bank. 
