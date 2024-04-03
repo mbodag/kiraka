22c96e47f6f4e66ae97fce3e354e828d524a0417
khigashinosg <108298890+khigashinosg@users.noreply.github.com>
2024-04-03 18:43:13 +0100

    changed requirements to match dev (matis' changes)

--- api/config.py
+++
@@ -1,5 +0,0 @@
-PASSWORD = 'A9zjFy3TwqHQbgx6!'
-USERNAME = 'root'
-DATABASE_NAME = 'kiraka_db'
-DATABASE_URI = f'mariadb+mariadbconnector://{USERNAME}:{PASSWORD}@localhost/{DATABASE_NAME}'
-PORT = 3230
