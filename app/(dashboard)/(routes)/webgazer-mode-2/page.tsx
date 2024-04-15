a4805613dbe85d4252bbc2fd9a42807f1e351bab
Hau, Jack C H <jack.hau23@imperial.ac.uk>
2024-04-15 14:55:12 +0100

    production ready

--- app/(dashboard)/(routes)/webgazer-mode-2/page.tsx
+++ app/(dashboard)/(routes)/webgazer-mode-2/page.tsx
@@ -1,45 +1,79 @@
 // pages/webgazer.tsx or a similar file
-import React from 'react';
-import DashboardLayout from '../layout';
+"use client";
+
+import React, { useEffect } from "react";
+import DashboardLayout from "../layout";
 import Mode2Display from "@/components/mode-2/mode-2-display";
-import { PracticeIDProvider } from '@/contexts/PracticeIDContext';
-import { SelectedTextProvider } from '@/contexts/SelectedTextContext'; // Adjust the import path if necessary
-import { auth, currentUser } from "@clerk/nextjs";
+import { PracticeIDProvider } from "@/contexts/PracticeIDContext";
+import { SelectedTextProvider } from "@/contexts/SelectedTextContext"; // Adjust the import path if necessary
+import { auth, currentUser, useAuth } from "@clerk/nextjs";
 
-
-export default async function WebgazerPage() {
-  const { userId } = auth();
-
-  const user = await currentUser();
-  const data = JSON.stringify({ user_id: user?.id });
-  console.log(data);
-
-  if (userId) {
-    try {
-      const response = await fetch(
-        "http://localhost:3000/api/store-user-data",
-        {
+export default function WebgazerPage() {
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+  useEffect(() => {
+    const sendDatatoServer = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/store-user-data`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ user_id: user?.id }),
+          body: JSON.stringify({ user_id: userId }),
+        });
+
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error! status: ${response.status} ${response.url}`
+          );
         }
-      );
 
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error sending data to server: ", error);
+      }
+    };
+    sendDatatoServer();
+  });
 
-  
+  // const { userId } = auth();
+
+  // const user = await currentUser();
+  // const data = JSON.stringify({ user_id: userId });
+  // console.log(data);
+
+  // if (userId) {
+  //   // console.log(userId);
+  //   try {
+  //     const response = await fetch(
+  //       "http://localhost:3000/api/store-user-data",
+  //       {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ user_id: userId }),
+  //       }
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error(
+  //         `HTTP error! status: ${response.status} ${response.url}`
+  //       );
+  //     }
+
+  //     const data = await response.json();
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
   return (
     <SelectedTextProvider>
       <PracticeIDProvider>
         <DashboardLayout navbarType="standard-auto">
-              <Mode2Display />
+          <Mode2Display />
         </DashboardLayout>
       </PracticeIDProvider>
     </SelectedTextProvider>
