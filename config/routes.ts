e86bd50e8efeb9f77837cccf6ce7fa3783c3fa1c
Matis Bodaghi <matisbodaghi@gmail.com>
2024-04-25 17:25:10 +0100

    Changed feedback form link

--- config/routes.ts
+++ config/routes.ts
@@ -4,7 +4,7 @@ const Routes = {
     UPLOAD: '/upload',
     INSTRUCTIONS: '/instructions',
     ANALYTICS: '/analytics',
-    FEEDBACK: 'https://forms.gle/nijvaqhDHYo3QE2v5'
+    FEEDBACK: 'https://forms.gle/EqSwybdkeWA2JW7m9'
   };
   
   export default Routes;
