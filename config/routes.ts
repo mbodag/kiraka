f8913430a74dd90491c6b214be08b4fa4f3af505
Matis Bodaghi <matisbodaghi@gmail.com>
2024-04-26 14:34:01 +0100

    Updated instructions and terms and conditions

--- config/routes.ts
+++ config/routes.ts
@@ -6,6 +6,7 @@ const Routes = {
     ANALYTICS: '/analytics',
     FEEDBACK: 'https://forms.gle/EqSwybdkeWA2JW7m9',
     QUIZ  : '/quiz',
+    DOCMODE: '/doc-mode',
   };
   
   export default Routes;
