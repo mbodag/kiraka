1dd2452826f6d985ac1683a80ae385905efbb750
macuser <fz221@ic.ac.uk>
2024-04-27 05:28:15 +0100

    Final adjustment of FlashMode popup.

--- components/flash-mode/flash-adaptive-display.tsx
+++ components/flash-mode/flash-adaptive-display.tsx
@@ -1,6 +1,6 @@
 "use client"; 
 
-import { useEffect, useState, useRef, useMemo, FC } from "react";
+import { useEffect, useState, useRef, FC } from "react";
 import { useSelectedText } from "@/contexts/SelectedTextContext";
 import CounterDisplay from "@/components/doc-mode/counter-display";
 import styles from '@/app/(dashboard)/(routes)/Dashboard.module.css';
@@ -116,6 +116,7 @@ const Mode2Display = () => {
             sessionStorage.setItem('isExistingSession', 'true'); // Mark this session as existing
         }
     }, []);
+    
     useEffect(() => {
         const fetchPastWPM = async () => {
           try {
@@ -823,10 +824,10 @@ const Mode2Display = () => {
                                         </p>
                                         <p>Explore two FlashMode configurations: <span style={{ color: 'darkblue' }}><strong>Static</strong></span> and <span style={{ color: 'darkgreen' }}><strong>Adaptive</strong></span>. To choose, use the toggle switch button above.</p>
                                         <div className='mx-2 mt-4 bg-blue-50 shadow-lg rounded-xl p-4'>
-                                        <p><strong>Static:</strong> Chunks of text successively appear in brief, rapid bursts or "flashes". Speed, measured in <span style={{ fontStyle: 'italic' }}>WPM (Words Per Minute)</span>, can be adjusted with the arrow keys. Otions to pause, start, or restart are also available at any time.</p>
+                                        <p><strong>Static:</strong> Chunks of text successively appear in brief, rapid bursts or "flashes". Speed, measured in <span style={{ fontStyle: 'italic' }}>WPM (Words Per Minute)</span>, can be adjusted with the arrow keys. Options to pause, start, or restart are also available at any time.</p>
                                         </div>
                                         <div className='mx-2 my-6 bg-green-50 shadow-lg rounded-xl p-4'>
-                                            <p><strong>Adaptive</strong> <span style={{ fontStyle: 'italic' }}>(Recommended)</span>: Integrates Webgazer's eye-tracking technology to automatically adjust your WPM, encouraging faster reading. <span style={{ textDecoration: 'underline'  }}>If pace feels too fast</span>, manual WPM adjustments via arrow keys and controls to pause, start, or restart are still available.</p>
+                                            <p><strong>Adaptive</strong> <span style={{ fontStyle: 'italic' }}>(Recommended)</span>: Integrates Webgazer's eye-tracking to automatically adjust your WPM, encouraging faster reading. <span style={{ textDecoration: 'underline'  }}>If the pace feels too fast</span>, manual WPM adjustments via arrow keys and controls to pause, start, or restart are still available.</p>
                                         </div>
                                         <p>To begin WebGazer calibration, click the button below!</p>
                                         <br></br>
