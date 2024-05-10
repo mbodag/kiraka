756bd0fa7a342aea36e7ba97eabb9130f7c29ce2
macuser <fz221@ic.ac.uk>
2024-05-10 05:42:08 +0100

    Improved the texts on the landing pages About and Contact.
    Former-commit-id: 5f4aa5a66239163a86999c8afd2b4f0f3c650ee1

--- app/(landing)/(routes)/about/page.tsx
+++ app/(landing)/(routes)/about/page.tsx
@@ -7,9 +7,10 @@ const AboutPage = () => {
       <div className="text-center">
         <h1 className="text-white text-7xl font-bold mb-12 typewriter" style={{ color: 'rgb(0, 139, 139)' }}>About Our Platform</h1>
         <p className="text-white text-lg max-w-xl mx-auto animate-gradientFadeIn landing-font">
-        Welcome to Kiraka.ai! This platform is part of our university project at <span style={{fontStyle: 'italic'}}>Imperial College London </span> 
-        aimed at exploring the potentials of speed reading and its effects on comprehension. 
-        We&apos;ve developed tools that dynamically adjust to your reading speed and style, continually challenging your limits.<br></br> <br></br> 
+        Welcome to Kiraka.ai! Our name, &apos;Kiraka&apos;, is inspired by the Arabic word 
+        for reading (qirā&apos;ah) and reflects our commitment to enhancing the way people engage with written content. <br /><br />
+        Initiated as a university project at <span style={{fontStyle: 'italic'}}>Imperial College London</span>, our platform explores the potential 
+        of speed reading and its impact on comprehension. We&apos;ve developed tools that dynamically adjust to your reading pace and style, continually challenging your limits. <br /><br />
         Join us in pushing the boundaries of how we absorb information!
         </p>
       </div>
--- app/(landing)/(routes)/pricing/page.tsx
+++ app/(landing)/(routes)/pricing/page.tsx
@@ -5,7 +5,7 @@ const PricingPage = () => {
   return (
     <div className="flex justify-center item-top h-screen" style={{ marginTop: '100px' }}>
       <div className="text-center">
-        <h1 className="text-white text-7xl font-bold mb-12 typewriter" style={{ color: 'rgb(220,220,200)', lineHeight: '1.2'}}>Our Pricing Plans</h1>
+        <h1 className="text-white text-7xl font-bold mb-12 typewriter" style={{ color: 'rgb(127, 200, 212)', lineHeight: '1.2'}}>Our Pricing Plans</h1>
         <p className="text-white text-lg max-w-xl mx-auto animate-gradientFadeIn landing-font">
           Our platform is freely available to all. We are dedicated to enhancing your reading experience 
           by providing innovative tools that help you read faster and more efficiently. Whether you&apos;re a 
